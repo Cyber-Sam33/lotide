0679e985899a59fc2f28fac5ef79b0df1cf70fe2
Cyber-Sam33 <issasammi@yahoo.com>
2023-01-09 22:47:04 +0000

    Head Function

--- head.js
+++ head.js
@@ -1,4 +1,7 @@
-// FUNCTION IMPLEMENTATION
+const head = function(array){
+  return array[0];
+}
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} equals ${expected}`);
@@ -7,8 +10,8 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Lighthouse", "Lighthouse");
-assertEqual(3, 7);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([3]), 3);
+assertEqual(head([]), 0);
+
