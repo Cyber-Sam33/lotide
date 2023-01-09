621ccb5d727864432727acdf38acca1334667d30
Cyber-Sam33 <issasammi@yahoo.com>
2023-01-09 23:14:41 +0000

    Tail Function

---
+++ tail.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} equals ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} does not equal ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  const tailArray = array.slice(1);
+  return tailArray;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+assertEqual(result.length, 2);
+
+console.log(tail([1, 2, 3, 4, 5]));
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words));//this creates a new tail array but leaves the original intact
+assertEqual(words.length, 3);// confirms original array still has 3 elements
