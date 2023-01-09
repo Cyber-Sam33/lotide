5938c5553ffe69ce21b60d140b1b83607ad5c8a0
Cyber-Sam33 <issasammi@yahoo.com>
2023-01-09 22:34:09 +0000

    Assert Equal Function - Template Literal

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🛑🛑🛑 Assertion Passed: " + actual + " equals " + expected);
+    console.log(` ✅✅✅ Assertion Passed: ${actual} equals ${expected}`);
   } else {
-    console.log("✅✅✅Assertion Failed: " + actual + " does not equal " + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} does not equal ${expected}`);
   }
 };
 
