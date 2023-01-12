fb45e2693f295f657f2d470aa23e8a2c32fc6605
Cyber-Sam33 <issasammi@yahoo.com>
2023-01-12 04:26:20 +0000

    eqObjects.js fixed

---
+++ assertObjectEqual.js
@@ -0,0 +1,12 @@
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect; 
+  if (actual === expected){
+    return console.log(`✅✅✅ Assertion Passed: ${actual} equals ${expected}`);
+  }
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} does not equal ${expected}`);
+
+
+};
+
+console.log(`Example label: ${inspect(actual)}`);
+
